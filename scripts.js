2fe27e1af40cfff78000b5a4ac19544f0c0d13f1
Maria Bajena <mbajena@vp.pl>
2017-02-25 15:07:32 +0100

    zad. 8.5

--- scripts.js
+++ scripts.js
@@ -1,15 +1,19 @@
-function Telefon(marka, cena, kolor) {
-	this.marka = marka; 
-	this.cena = cena;
-	this.kolor = kolor;
+function Button(text) {
+	this.text = text || "Hello";
 }
 
-Telefon.prototype.printInfo = function() {
-		console.log("Marka telefonu to: "  + this.marka + ", kolor to " + this.kolor + ", a cena to " + this.cena + ".”");
+Button.prototype = {
+	create: function() {
+		var self = this;
+		this.$element = $("<button>");
+		this.$element.text(this.text);
+		this.$element.click(function() {
+			alert(self.text);
+		});
+		$('body').append(this.$element);
+
+	}
 }
 
-var iPhone6S = new Telefon("Apple", 2250, "srebrny"),
-	SamsungGalaxyS6 = new Telefon("Samsung", 2000, "czarny"),
-	OnePlusOne = new Telefon("One Plus", 1800, "biały");
-
-iPhone6S.printInfo();
+var btn1 = new Button('Witam!');
+btn1.create(); 
